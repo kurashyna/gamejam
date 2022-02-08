d9809774c2705eac5e1227c53b4b59fb5e9d53f3
Pouks <poulbleu.du38@gmail.com>
2022-02-08 12:02:54 +0100

    jsp

--- player.py
+++ player.py
@@ -41,8 +41,6 @@ class Player:
     def move(self):
         self.game.terrain.move(-(self.velocity[0] *
                                self.speed), -(self.velocity[1] * self.speed))
-        # self.rect.move_ip(
-        #     self.velocity[0] * self.speed, self.velocity[1] * self.speed)
 
     def bounce(self):
         self.velocity[0] = -self.velocity[0]
